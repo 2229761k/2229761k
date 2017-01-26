a42eb081265f8e41639a12efad960162d1bb99e8
Dohyung Kim <2229761k@student.gla.ac.uk>
2017-01-26 18:11:46 +0000

    26/01/2017

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from rango.models import Category, Page
+
+admin.site.register(Category)
+admin.site.register(Page)
+
+#class Category(models.Model):
+#    name = models.CharField(max_length=128, unique=True)
+
+#    class Meta:
+#        verbose_name_plural = 'Categorys'
+
+#    def __str__(self):
+#        return self.name
+
+
 
 # Register your models here.
