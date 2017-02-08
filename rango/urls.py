3e52c7812f9a0e2ce76003c525dcedaaf5942378
Dohyung Kim <2229761k@student.gla.ac.uk>
2017-02-08 18:30:05 +0000

    08/02/2017

--- rango/urls.py
+++ rango/urls.py
@@ -15,19 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from rango import views
-from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
-from rango import views
 
-app_name = 'rango'
+app_name='rango'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
-    url(r'^page/(?P<page_name_slug>[\w\-]+)/$',
-        views.show_page, name='show_page'),
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
         views.add_page, name='add_page'),
