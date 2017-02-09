b10bdd6e0245f6257ce08862844d81fc8f683d15
Dohyung Kim <2229761k@student.gla.ac.uk>
2017-02-09 16:40:16 +0000

    09/02/2017

--- populate_rango.py
+++ populate_rango.py
@@ -44,7 +44,7 @@ def populate():
          "url": "http://flask.pocoo.org",
          "views": 12}]
 
-    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+    cats = {"Python": {"pages": python_pages, "views": 0, "likes": 64},
             "Django": {"pages": django_pages, "views": 64, "likes": 32},
             "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}}
 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -13,12 +13,13 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
-STATIC_URL = '/static/'
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [STATIC_DIR]
+
+STATIC_URL = '/static/'
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 LOGIN_URL = '/rango/login/'
